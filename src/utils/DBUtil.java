338395c066a05494037ae27eaf535905049125b3
misaki.ito <misaki@allexceed.co.jp>
2021-06-01 10:21:35 +0900

    Add database files

---
+++ src/utils/DBUtil.java
@@ -0,0 +1,22 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    private static final String PERSISTENCE_UNIT_NAME = "task";
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createEntityManager(){
+        return getEantityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory getEantityManagerFactory(){
+        if(emf == null){
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return emf;
+    }
+
+}
