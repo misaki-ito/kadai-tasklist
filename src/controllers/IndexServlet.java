b18ee92c7098f39f7a12e16eead4e224603d5541
misaki.ito <misaki@allexceed.co.jp>
2021-06-01 16:42:37 +0900

    modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,10 +25,38 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        /*int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException e){}
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class)
+                              .setFirstResult(10 * (page - 1))
+                              .setMaxResults(10)
+                              .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTaskCount",Long.class)
+                                     .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("tasks",tasks);
+        request.setAttribute("tasks_count",tasks_count);
+        request.setAttribute("page",page);
+
+        if(request.getSession().getAttribute("flush") != null){
+            request.setAttribute("flush",request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flesh");
+        }*/
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request,response);
     }
 
     /*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
